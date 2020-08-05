682f8504c65f4cad306678507028bea538496f5f
stas <stanislav.chekmenev@gmail.com>
2020-08-05 09:54:34 +0200

    latest changes

--- pycharm_debugging/1_exercise.py
+++ pycharm_debugging/1_exercise.py
@@ -7,18 +7,14 @@ from toy_modules.train import train
 
 if __name__ == '__main__':
 
-    # Create two metric objects to calculate mean loss across batches
-    train_loss = tf.keras.metrics.Mean(name='train_loss')
-    test_loss = tf.keras.metrics.Mean(name='test_loss')
-
-    # Create an optimizer
-    optimizer = tf.keras.optimizers.SGD(learning_rate=0.001, momentum=0.9)
-
     # Generate datasets
     train_dataset, test_dataset = generate_datasets()
 
     # Create the model
-    regressor = RegressorNet(input_shape=40, optimizer=optimizer)
+    regressor = RegressorNet(
+        input_shape=20,
+        optimizer=tf.keras.optimizers.SGD(learning_rate=0.001, momentum=0.9)
+    )
 
     # Train (leave the number of epochs unchanged and equal to 200)
     train(
@@ -26,7 +22,5 @@ if __name__ == '__main__':
         epochs=200,
         train_dataset=train_dataset,
         test_dataset=test_dataset,
-        train_loss=train_loss,
-        test_loss=test_loss,
-        save_dir='test1'
+        save_dir='run1'
     )
