31dd46978bbc64bc3e024221a57f0849c1c037ce
stanislav-chekmenev <stanislav.chekmenev@gmail.com>
2025-11-06 13:45:58 +0100

    change error msg

--- project/test_env.py
+++ project/test_env.py
@@ -5,4 +5,4 @@ try:
     print("Torch installed successfully. Torch version:", torch.__version__)
     print("Scikit-learn installed successfully. Scikit-learn version:", sklearn.__version__)
 except ImportError:
-    raise ImportError("Please fix your PyTorch installation.")
+    raise ImportError("Please fix your PyTorch and/or Scikit-learn installation.")
