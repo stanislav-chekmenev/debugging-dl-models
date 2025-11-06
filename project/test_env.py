8d62151f71c5d3c50351788059ba3134ec28bc33
stanislav-chekmenev <stanislav.chekmenev@gmail.com>
2025-11-06 13:48:05 +0100

    split import tests

--- project/test_env.py
+++ project/test_env.py
@@ -1,8 +1,13 @@
 try:
     import torch
-    import sklearn
 
     print("Torch installed successfully. Torch version:", torch.__version__)
+except ImportError:
+    raise ImportError("Please fix your PyTorch installation.")
+
+try:
+    import sklearn
+
     print("Scikit-learn installed successfully. Scikit-learn version:", sklearn.__version__)
 except ImportError:
-    raise ImportError("Please fix your PyTorch and/or Scikit-learn installation.")
+    raise ImportError("Please fix your Scikit-learn installation.")
